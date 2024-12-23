4efab700a46ea4ce8f83bd4b20b2f9f2b87b9184
Greg Poirier <greg.istehbest@gmail.com>
2024-12-23 16:25:28 -0600

    Use settings in alembic environment

--- alembic/env.py
+++ alembic/env.py
@@ -5,19 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from dotenv import load_dotenv
-import os
-
-from app.database import DATABASE_URL
-
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+from app.core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
